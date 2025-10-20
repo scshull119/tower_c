51512918fced32d7a08415952b23618f78a4e422
Samuel Shull <samuel.c.shull@gmail.com>
2025-10-20 16:30:18 -0500

    add raylib dependency and set up basic window

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "raylib.h"
 
 int main(void) {
-    printf("Hello, World!\n");
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 450;
+
+    printf("Tower C Railroad Simulator\n");
+    printf("Version 0.1\n");
+
+    InitWindow(screenWidth, screenHeight, "Tower C Railroad Simulator");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Tower C Railroad Simulator", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
