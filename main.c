0dcc08a3b79825e8d83170f40205e544e2a42b9b
Samuel Shull <samuel.c.shull@gmail.com>
2025-10-20 17:31:35 -0500

    break out functions for model loading and rendering

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "raylib.h"
 
-int main(void) {
+void loadMstsModel() {
+    // Loads an MSTS model and prepares it for rendering by raylib
+    printf("Loading MSTS model...\n");
+}
+
+void renderWindow() {
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 450;
-
-    printf("Tower C Railroad Simulator\n");
-    printf("Version 0.1\n");
 
     InitWindow(screenWidth, screenHeight, "Tower C Railroad Simulator");
     SetTargetFPS(60);
@@ -19,6 +21,16 @@ int main(void) {
     }
 
     CloseWindow();
+}
+
+int main(void) {
+    printf("Tower C Railroad Simulator\n");
+    printf("Version 0.1\n");
+
+    loadMstsModel();
+    // renderWindow();
+
+    printf("TCRS exited.\n");
 
     return 0;
 }
